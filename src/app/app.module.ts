716cbb40ce0dde6d03e483ad75e0439fe10db087
Adriano Raiano <adriano@raiano.ch>
2021-06-10 18:42:38 +0200

    more goodies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { I18NEXT_SERVICE, I18NextModule, I18NextLoadResult, ITranslationService, defaultInterpolationFormat  } from 'angular-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import LocizeApi from 'i18next-locize-backend';
+import LastUsed from 'locize-lastused';
+import { locizePlugin } from 'locize';
 
 import { AppComponent } from './app.component';
+
+const locizeOptions = {
+  projectId: 'c0fa7b2a-219b-44c8-8672-e386b8123b21',
+  // apiKey: 'my-api-key' // used for handleMissing functionality, do not add your api-key in a production buildyour
+};
 
 const i18nextOptions = {
   debug: true,
   fallbackLng: 'en',
-  backend: {
-    projectId: 'c0fa7b2a-219b-44c8-8672-e386b8123b21'
-  },
+  // saveMissing: true,
+  backend: locizeOptions,
+  locizeLastUsed: locizeOptions,
   interpolation: {
     format: I18NextModule.interpolationFormat(defaultInterpolationFormat)
   }
@@ -20,6 +27,17 @@ const i18nextOptions = {
 export function appInit(i18next: ITranslationService) {
   return () => {
     let promise: Promise<I18NextLoadResult> = i18next
+      // locize-lastused
+      // sets a timestamp of last access on every translation segment on locize
+      // -> safely remove the ones not being touched for weeks/months
+      // https://github.com/locize/locize-lastused
+      .use(LastUsed)
+      // locize-editor
+      // InContext Editor of locize
+      .use(locizePlugin)
+      // i18next-locize-backend
+      // loads translations from your project, saves new keys to it (saveMissing: true)
+      // https://github.com/locize/i18next-locize-backend
       .use(LocizeApi)
       .use<any>(LanguageDetector)
       .init(i18nextOptions);
